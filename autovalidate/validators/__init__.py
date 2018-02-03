746f04e199a1a648004c430bc64031aebd8ae6f9
Dan Tao <daniel.tao@gmail.com>
2018-02-03 10:53:25 -0600

    Consolidate two import lines into one

--- autovalidate/validators/__init__.py
+++ autovalidate/validators/__init__.py
@@ -1,6 +1,5 @@
 from autovalidate.validators.core import (ValidationResult, get_validator,
                                           validates)
-from autovalidate.validators import json_ as json
-from autovalidate.validators import yaml_ as yaml
+from autovalidate.validators import json_ as json, yaml_ as yaml
 
 __all__ = ['ValidationResult', 'get_validator', 'json', 'validates', 'yaml']

f435bca02bc925cd63c54554230d08ead2f7704d
Dan Tao <daniel.tao@gmail.com>
2018-02-03 10:58:39 -0600

    Output extra line before summarizing results in "dot" reporter

--- autovalidate/reporters/dot.py
+++ autovalidate/reporters/dot.py
@@ -5,3 +5,7 @@ from autovalidate.reporters.core import Reporter, reporter
 class DotReporter(Reporter):
     def report(self, result):
         self.out.write('.' if result.valid else 'F')
+
+    def summarize(self):
+        self.out.write('\n')
+        super(DotReporter, self).summarize()
